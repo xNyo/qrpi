6806085e3d36f3057bd3545f9a1a44c464d7e390
Giuseppe Guerra <to@nyo.zz.mu>
2017-06-27 17:20:42 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# qrpi
+*qrpi* is a little qr code generator web api written in Python with asyncio and
+[qrcode](https://github.com/lincolnloop/python-qrcode), in a single file
+
+## Requirements
+- Python 3.6
+
+## Installation
+```
+# get the source code
+$ git clone ...
+$ cd qrpi
+
+# customize web server host, port and rate limit
+# (optional, the api will use its default settings
+# if settings.ini is not present in the current directory)
+$ cp settings.sample.ini settings.ini
+$ nano settings.ini
+...
+
+# create a virtualenv and install the required dependencies
+$ virtualenv -p $(which python3.6) .pyenv
+$ source .pyenv/bin/activate
+(.pyenv) $ pip install -r requirements.txt
+
+# start the web server (nohup/tmux needed for production)
+(.pyenv) $ python3 qrpi.py
+======== Running on http://0.0.0.0:8833 ========
+(Press CTRL+C to quit)
+```
+
+## Usage
+When you start the program, an aiohttp server will start on the port you've set in the config file (default: `8833`).
+There's only one API handler (`/`), and it accepts there GET parameters:
+
+### GET parameters
+Name               | Description                                                       | Required? | Default
+-------------------|-------------------------------------------------------------------|---------- | --------
+`data`             | QR Code's content                                                 | Yes       | -
+`version`          | QR Code version (size), from 1 to 40. 1 = smallest (21x21 matrix) | No        | Auto detected
+`error_correction` | QR Code error correction (`l`,`m`,`q`,`h`)                        | No        | `m`
+`box_size`         | Number of pixels of each QR Code 'box'                            | No        | Auto detected
+`border`           | Number of 'boxes' empty from the border (min 4)                   | No        | `4`
+`base64`           | Returns a base64 data URL if `1`, else it returns an image        | No        | `0`
+
+### Response
+The response can be either a JPG image (`Content-Type: image/jpeg;`) or a JSON Object (`Content-Type: application/json;`).
+A JSON object will be returned if there's an error or if the `base64` parameter is `1`. In all other cases, a JPEG
+image will be returned. In case of a JSON object, it'll always contain a `status` key, that complies with the HTTP
+status code. If there's an error a `message` key that explains the error will be present in the JSON response too.
+
+Name      | Description                  | Always present?
+----------|------------------------------|--------------------------------
+`status`  | Status code                  | Yes
+`message` | Message explaining the error | No, only if there was an error
+`data`    | Base64 data URL              | No, only if `base64 = 1`
+
+### Status codes
+
+Status code | Description
+------------|--------------------------------
+`200`       | Request processed successfully
+`400`       | Missing or invalid parameter
+`429`       | Rate limit exceeded
+`500`       | Internal server error
+
+## Rate limit
+The default rate limit is 60 request per minute from a single IP address. The rate limit can be adjusted or entirely
+disabled from the settings file.
+
+## Deploying
+One way of deploying the api is using nginx as a reverse proxy.
+This is a sample nginx config for qrpi:
+```
+server {
+    server_name qrpi.mydomain.com;
+    listen 80;
+    charset utf-8;
+
+    location / {
+        proxy_pass http://localhost:8833;
+    }
+}
+```
+The api checks the IP address from the `CF-Connecting-IP` and `X-Forwarded-For` headers if possible,
+so you don't need any extra configuration if you're using Cloudflare.
+
+## License
+MIT
---
+++ qrpi.py
@@ -0,0 +1,242 @@
+import io
+import base64
+
+import asyncio
+from aiohttp import web
+
+import qrcode
+from decouple import config
+import time
+
+
+class RateLimiterClient:
+    def __init__(self, rate, per):
+        """
+        Initialise a rate limiter for a single client
+
+        :param rate: rate limit
+        :param per: check rate
+        """
+        self.rate = rate
+        self.per = per
+        self.allowance = self.rate  # requests
+        self.last_check = time.time()
+
+    def check(self, increase=True):
+        """
+        Checks if this client is above the rate limit
+
+        :param increase: if True, increase the requests number
+        :return: True if the client is allowed,
+                 False if the client has surpassed the rate limit
+        """
+        if not CONFIG["RATE_LIMIT"]:
+            return True
+        current = time.time()
+        time_passed = current - self.last_check
+        if increase:
+            self.last_check = current
+        self.allowance += time_passed * (self.rate / self.per)
+        if self.allowance > self.rate:
+            self.allowance = self.rate  # throttle
+        if self.allowance < 1.0:
+            return False
+        else:
+            if increase:
+                self.allowance -= 1.0
+            return True
+
+
+class RateLimiter:
+    def __init__(self, rate, per):
+        """
+        Initialise a group of RateLimiterClient
+
+        :param rate: rate limit
+        :param per: check rate
+        """
+        self.rate = rate
+        self.per = per
+        self.clients = {}
+
+    def check(self, client_id, increase=True):
+        """
+        Checks if a client is above the rate limit.
+        If the client doesn't exist, a new RateLimiterClient object
+        is created and added to clients dictionary.
+
+        :param client_id: client identifier
+        :param increase: if True, increase the requests number
+        :return: True if the client is allowed,
+                 False if the client has surpassed the rate limit
+        """
+        if client_id not in self.clients:
+            self.clients[client_id] = RateLimiterClient(self.rate, self.per)
+        return self.clients[client_id].check(increase)
+
+CONFIG = {
+    "WEB_HOST": config("WEB_HOST", default="127.0.0.1"),
+    "WEB_PORT": config("WEB_PORT", default="8833", cast=int),
+    "RATE_LIMIT": config("RATE_LIMIT", default="1", cast=bool),
+    "RATE_LIMIT_RATE": config("RATE_LIMIT_RATE", default="60", cast=int),
+    "RATE_LIMIT_PER": config("RATE_LIMIT_PER", default="60", cast=int),
+}
+RATE_LIMITER = RateLimiter(rate=CONFIG["RATE_LIMIT_RATE"], per=CONFIG["RATE_LIMIT_PER"])
+
+
+class InvalidArgumentError(Exception):
+    def __init__(self, arg_name):
+        self.arg_name = arg_name
+
+
+def async_run(func, _ioloop=None, *args, **kwargs):
+    """
+    Run a sychronous function asynchronously.
+    Usage:
+    ```
+    await async_run(func, arg1, arg2, kwarg1=1)
+    ```
+
+    :param func: function object to run (not function call)
+    :param _ioloop: IOLoop
+    :param args: function arguments
+    :param kwargs: function keyword arguments
+    :return: future
+    """
+    if _ioloop is None:
+        _ioloop = asyncio.get_event_loop()
+    return _ioloop.run_in_executor(None, func, *args)
+
+
+def get_arg(request, name, default, acceptable=None):
+    """
+    Checks if an argument is passed as a GET parameter
+    to a `aiohttp.web.Request` object and if it is valid
+
+    :param request: `aiohttp.web.Request` object
+    :param name: name of the GET parameter
+    :param default: default value if the parameter was not passed
+    :param acceptable: acceptable values. Can be `int`, `bool` or a `list`.
+                       - If `int`, digit-only strings will be considered valid and function
+                       will return the parameter already casted to `int`;
+                       - If `bool`, `1`, `yes`, `y`, and `true` will be treated as `True`
+                       and `0`, `no`, `n`, and `false` will be treated as `False`.
+                       All other values are be considered invalid;
+                       - If `list`, all values inside `acceptable` are considered valid
+                       (case insensitive)
+    :return: GET argument (if provided), or default one if it's not present.
+             Returns an `int` if `acceptable = int`, `bool` if `acceptable = bool`,
+             otherwise `str`
+    :raises: `InvalidArgumentError()` if the argument is not valid.
+    """
+    if name in request.GET:
+        # Argument provided, check if it's valid
+        if acceptable is not None:
+            if acceptable is int:
+                if request.GET[name].isdigit():
+                    return int(request.GET[name])
+                raise InvalidArgumentError(name)
+            elif acceptable is list and request.GET[name].lower() not in acceptable:
+                raise InvalidArgumentError(name)
+            elif acceptable is bool and request.GET[name]:
+                if request.GET[name].lower() in ["1", "yes", "0", "no", "y", "n", "true", "false"]:
+                    return request.GET[name].lower() in ["1", "yes", "y", "true"]
+                raise InvalidArgumentError(name)
+        return request.GET[name]
+    else:
+        # Argument not provided, return the default one
+        return default
+
+
+def get_ip(request):
+    """
+    Get request's IP adddress
+
+    :param request: aiohttp request
+    :return:
+    """
+    if "CF-Connecting-IP" in request.headers:
+        return request.headers["CF-Connecting-IP"]
+    elif "X-Forwarded-For" in request.headers:
+        return request.headers["X-Forwarded-For"]
+    else:
+        return request.transport.get_extra_info("peername"),
+
+async def qr(request):
+    """
+    QR Code generator handler
+
+    :param request:
+    :return:
+    """
+    try:
+        # Rate limit check
+        if not RATE_LIMITER.check(get_ip(request)):
+            return web.json_response({
+                "status": 429,
+                "message": "Rate limit exceeded, slow down!"
+            })
+
+        # Make sure `data` was passed
+        if "data" not in request.GET:
+            return web.json_response({
+                "status": 400,
+                "message": "Missing required `data` GET argument"
+            }, status=400)
+
+        # Get options
+        version = get_arg(request, "version", None, int)
+        error_correction = get_arg(request, "error_correction", "m", [
+            "m", "h", "l", "q"
+        ])
+        error_correction = {
+            "m": qrcode.constants.ERROR_CORRECT_M,
+            "h": qrcode.constants.ERROR_CORRECT_H,
+            "l": qrcode.constants.ERROR_CORRECT_L,
+            "q": qrcode.constants.ERROR_CORRECT_Q,
+        }[error_correction]
+        box_size = get_arg(request, "box_size", 10, int)
+        border = get_arg(request, "border", 4, int)
+        as_base64 = get_arg(request, "base64", 0, bool)
+
+        # Generate QR Code with provided options
+        qr = qrcode.QRCode(
+            version=version,
+            error_correction=error_correction,
+            box_size=box_size,
+            border=border
+        )
+        qr.add_data(request.GET["data"])
+        await async_run(qr.make, fit=True)
+
+        # Make image
+        img = await async_run(qr.make_image)
+        output = io.BytesIO()
+        img.save(output)
+        image_data = output.getvalue()
+
+        # Return qr code as base64
+        if as_base64:
+            data_url = "data:image/jpg;base64," + base64.b64encode(image_data).decode()
+            return web.json_response({
+                "status": 200,
+                "data": data_url
+            }, status=200)
+
+        # Or return it as a normal image
+        return web.Response(body=image_data, content_type="image/jpeg")
+    except InvalidArgumentError as e:
+        return web.json_response({
+            "status": 400,
+            "message": "Invalid argument value ({})".format(e.arg_name)
+        }, status=400)
+
+
+def main():
+    app = web.Application()
+    app.router.add_get("/", qr)
+    web.run_app(app, host=CONFIG["WEB_HOST"], port=CONFIG["WEB_PORT"])
+
+
+if __name__ == "__main__":
+    main()
